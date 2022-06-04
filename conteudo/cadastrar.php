bb11d3a989c6a53dd1c9842a53a6b26f474d7859
Kleber M Kardel <molinakardel@gmail.com>
2022-06-04 17:20:24 -0300

    Cria código PHP para inserção dos dados do usuário no banco de dados fazendo todas as validações necessárias

--- conteudo/cadastrar.php
+++ conteudo/cadastrar.php
@@ -2,9 +2,90 @@
 
     include("classe/conexao.php");
 
+    if(isset($_POST['confirmar'])) { //Verifica se usuário clicou no botão salvar para cadastrar um novo usuário
+
+        // 1- Registro dos dados
+        if(!isset($_SESSION)) 
+            session_start();
+
+        foreach($_POST as $chave=>$valor)
+            $_SESSION[$chave] = 
+            $mysqli->real_escape_string($valor); //Mantém os dados preenchidos caso saia da página ou ocorra algo de diferente
+
+        // 2- Validação dos dados
+
+          // strlen verifica tamanho da string informada (se estiver vazia (nula), apresenta menssagem de erro)
+        if(strlen($_SESSION['nome']) == 0)
+            $erro[] = "Preencha o nome do usuário.";
+
+        if(strlen($_SESSION['sobrenome']) == 0)
+            $erro[] = "Preencha o sobrenome do usuário";
+
+        if(substr_count($_SESSION['email'], '@') != 1 || substr_count($_SESSION['email'], '.') < 1 || substr_count($_SESSION['email'], '.') > 2) // Valida e-mail verificando se possui mais de um '@' e menos de um ou mais de 2 '.'
+            $erro[] = "Preencha o e-mail corretamente";
+
+        if(strlen($_SESSION['niveldeacesso']) == 0)
+            $erro[] = "Informe o Nível de Acesso do usuário";
+
+        if(strlen($_SESSION['senha']) < 8 || strlen($_SESSION['senha']) > 16)
+            $erro[] = "Preencha a senha corretamente";
+
+        if(strcmp($_SESSION['senha'], $_SESSION['resenha']) != 0) //Verifica se senha e repita a senha estão iguais
+            $erro[] = "As senhas não conferem";
+
+        // 3- Inserção dos dados no banco e redireciona para tela de visualização de Usuários
+        if(count($erro) == 0){
+
+            $sql_code = "INSERT INTO usuarios (
+                nome, 
+                sobrenone, 
+                email, 
+                senha, 
+                sexo, 
+                niveldeacesso, 
+                dtCadastro)
+                VALUES(
+                '$_SESSION[nome]',
+                '$_SESSION[sobrenome]',
+                '$_SESSION[email]',
+                '$_SESSION[senha]',
+                '$_SESSION[sexo]',
+                '$_SESSION[niveldeacesso]',
+                '$_SESSION[dtCadastro]'
+                )";
+
+            $confirma = $mysqli->query($sql_code) or die($mysqli->error);
+
+            if($confirma){
+                unset($_SESSION['nome'],
+                $_SESSION['sobrenome'],
+                $_SESSION['email'],
+                $_SESSION['senha'],
+                $_SESSION['sexo'],
+                $_SESSION['niveldeacesso'],
+                $_SESSION['dtCadastro']);
+
+                echo "<script> location.href='index.php?p=inicial';</script>";
+
+            }else
+                $erro[] = $confirma;
+                    
+        }
+
+    }
+
 ?>
 
 <h1>Cadastrar Usuário</h1>
+<?php
+ if(count($erro) > 0){ //Mostra o erro que ocorreu
+     echo "<div class='erro'>"; 
+     foreach($erro as $valor) 
+        echo "$valor <br>"; 
+    
+    echo "</div>";
+}
+?>
 <a href="index.php?p=inicial">< Voltar</a>
 <form action="index.php?p=cadastrar" method="POST">
 
@@ -37,6 +118,7 @@
     <p class="espaco"></p>
 
     <label for="senha">Senha</label>
+    A senha deve conter entre 8 e 16 caracteres.
     <input name="senha" value="" required type="password">
     <p class="espaco"></p>
 
