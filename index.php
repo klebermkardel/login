4c0808cc6f5380e2d5e4bd957b67be83b4e7661c
Kleber M Kardel <molinakardel@gmail.com>
2022-06-04 15:40:57 -0300

    Criado estrutura HTML e CSS

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/styles.css">
     <title>Controle de Usuários</title>
 </head>
 <body>
-    
+    <div class="principal">
+
+    </div>
 </body>
 </html>
