4c665642a139e31c99f69bde9194091dc7a1e9c1
Kleber M Kardel <molinakardel@gmail.com>
2022-06-05 16:58:58 -0300

    Cria loop na página inicial.php que passa por todos os usuários cadastros no banco apresentando-os na tabela

--- conteudo/inicial.php
+++ conteudo/inicial.php
@@ -1,5 +1,11 @@
 <?php
+
     include("classe/conexao.php");
+
+    $sql_code = "SELECT * FROM usuarios";
+    $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
+    $linha = $sql_query->fetch_assoc();
+
 ?>
 
 <h1>Usuários</h1>
@@ -15,16 +21,20 @@
         <td>Data de Cadastro</td>
         <td>Ação</td>
     </tr>
+    <?php
+    do{
+    ?>
     <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?php echo $linha['nome']; ?></td>
+        <td><?php echo $linha['sobrenome']; ?></td>
+        <td><?php echo $linha['sexo']; ?></td>
+        <td><?php echo $linha['email']; ?></td>
+        <td><?php echo $linha['niveldeacesso']; ?></td>
+        <td><?php echo $linha['dtCadastro']; ?></td>
         <td>
-            <a href="index.php?p=editar&usuario=">Editar</a>
-            <a href="index.php?p=">Deletar</a>
+            <a href="index.php?p=editar&usuario=<?php echo $linha['codigo']; ?>">Editar</a>
+            <a href="index.php?p=deletar&usuario=<?php echo $linha['codigo']; ?>">Deletar</a>
         </td>
     </tr>
+    <?php } while($linha = $sql_query->fetch_assoc()); ?>
 </table>
