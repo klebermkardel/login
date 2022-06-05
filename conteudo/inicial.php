102a0424ba784c8c5c340e1c3187ca01c19da0f1
Kleber M Kardel <molinakardel@gmail.com>
2022-06-05 18:16:59 -0300

    Criado código para deletar usuários

--- conteudo/inicial.php
+++ conteudo/inicial.php
@@ -1,51 +1,53 @@
 <?php
 
-    include("classe/conexao.php");
+	include("classe/conexao.php");
 
-    $sql_code = "SELECT * FROM usuarios";
-    $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
-    $linha = $sql_query->fetch_assoc();
+	$sql_code = "SELECT * FROM usuarios";
+	$sql_query = $mysqli->query($sql_code) or die($mysqli->error);
+	$linha = $sql_query->fetch_assoc();
 
-    $sexo[1] = "Masculino";
-    $sexo[2] = "Feminino";
+	$sexo[1] = "Masculino";
+	$sexo[2] = "Feminino";
 
-    $niveldeacesso[1] = "Admin";
-    $niveldeacesso[2] = "Básico";
+	$niveldeacesso[1] = "Básico";
+	$niveldeacesso[2] = "Admin";
 
 ?>
 
 <h1>Usuários</h1>
-<a href="index.php?p=cadastrar">Cadastrar novo usuário</a>
-<p class="espaco"></p>
+<a href="index.php?p=cadastrar">Cadastrar um usuário</a>
+<p class=espaco></p>
 <table border=1 cellpadding=10>
-    <tr class="titulo">
-        <td>Nome</td>
-        <td>Sobrenome</td>
-        <td>Sexo</td>
-        <td>E-mail</td>
-        <td>Nível de Acesso</td>
-        <td>Data de Cadastro</td>
-        <td>Ação</td>
-    </tr>
-    <?php
-    do{
-    ?>
-    <tr>
-        <td><?php echo $linha['nome']; ?></td>
-        <td><?php echo $linha['sobrenome']; ?></td>
-        <td><?php echo $sexo[$linha['sexo']]; ?></td>
-        <td><?php echo $linha['email']; ?></td>
-        <td><?php echo $niveldeacesso[$linha['niveldeacesso']]; ?></td>
-        <td><?php 
-        $d = explode(" ", $linha['dtCadastro']);
-        $data = explode("-", $d[0]);
-        echo "$data[2]/$data[1]/$data[0] às $d[1]"; 
-        
-        ?></td>
-        <td>
-            <a href="index.php?p=editar&usuario=<?php echo $linha['codigo']; ?>">Editar</a> |
-            <a href="index.php?p=deletar&usuario=<?php echo $linha['codigo']; ?>">Deletar</a>
-        </td>
-    </tr>
-    <?php } while($linha = $sql_query->fetch_assoc()); ?>
+	<tr class=titulo>
+		<td>Nome</td>
+		<td>Sobrenome</td>
+		<td>Sexo</td>
+		<td>E-mail</td>
+		<td>Nível de Acesso</td>
+		<td>Data de Cadastro</td>
+		<td>Ação</td>
+	</tr>
+	<?php
+	do{
+	?>
+	<tr>
+		<td><?php echo $linha['nome']; ?></td>
+		<td><?php echo $linha['sobrenome']; ?></td>
+		<td><?php echo $sexo[$linha['sexo']]; ?></td>
+		<td><?php echo $linha['email']; ?></td>
+		<td><?php echo $niveldeacesso[$linha['niveldeacesso']]; ?></td>
+		<td><?php
+		$d = explode(" ", $linha['dtCadastro']);
+		$data = explode("-", $d[0]);
+		echo "$data[2]/$data[1]/$data[0] às $d[1]"; 
+
+		?></td>
+		<td>
+			<a href="index.php?p=editar&usuario=<?php echo $linha['codigo']; ?>">Editar</a> |
+			<a href="javascript: if(confirm('Tem certeza que deseja deletar o usuário <?php echo $linha['nome']; ?>?'))
+			location.href='index.php?p=deletar&usuario=<?php echo $linha['codigo']; ?>';">Deletar</a>
+		</td>
+	</tr>
+	<?php } while($linha = $sql_query->fetch_assoc()); ?>
+
 </table>
